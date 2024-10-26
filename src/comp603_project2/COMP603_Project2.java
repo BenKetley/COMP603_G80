bd65ecfbfec78fb9c3a8d6da08296f0d61cf277a
RoryWhateley <rorywhateley@gmail.com>
2024-10-27 12:51:00 +1300

    database

--- src/comp603_project2/COMP603_Project2.java
+++ src/comp603_project2/COMP603_Project2.java
@@ -7,28 +7,22 @@ package comp603_project2;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Ben
- */
 public class COMP603_Project2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        String boardSize;   //defining varibles 
+        // Initialize the database
+        BattleshipDatabase battleshipDatabase = new BattleshipDatabase();
+
+        String boardSize;
         int boardSizeint = 0;
         int shipCount;
         boolean player = true;
         boolean game = true;
-        Scanner scan = new Scanner(System.in);                                  //creating scanner
+        Scanner scan = new Scanner(System.in);
+
         do {
-            System.out.println("Enter Board Size between 2 - 26 or '1' to EXIT: ");                           //creates a gameboard based on user input sizes
+            System.out.println("Enter Board Size between 2 - 26 or '1' to EXIT: ");
             boardSize = scan.nextLine();
-            System.out.println((boardSize.charAt(0)));
-            //System.out.println((boardSize.charAt(0)));
             if (boardSize.length() == 2){
                 boardSizeint = (((boardSize.charAt(0) - '0')*10) + (boardSize.charAt(1) - '0'));
             }
@@ -41,28 +35,42 @@ public class COMP603_Project2 {
             }
 
         } while ((boardSizeint < 2 || boardSizeint > 26));
-        shipCount = boardSizeint - 1;                                              //sets the amount of ships to be -1 from board size
+        
+        shipCount = boardSizeint - 1;
 
-        GameBoardHome gameBoardHome = new GameBoardHome();                      //calling class functions
+        GameBoardHome gameBoardHome = new GameBoardHome();
         GameBoardAway gameBoardAway = new GameBoardAway();
+        
         gameBoardHome.initializeBoard(boardSizeint, shipCount);
         gameBoardHome.placeShip(boardSizeint, shipCount);
         System.out.println("BATTLESHIPS ROUND: 1" + "\n");
         gameBoardAway.initializeBoard(boardSizeint, shipCount);
 
+        // Example of using the database to save board size and ship count
+        try {
+            battleshipDatabase.statement.executeUpdate("INSERT INTO BattleShips (BoardSize, HIT, MISS) VALUES (" 
+                                                       + boardSizeint + ", 'False', 'True')");
+        } catch (Exception e) {
+            System.out.println("Error inserting data into the database: " + e.getMessage());
+        }
+
+        // Gameplay loop
         gameBoardAway.playGame(boardSizeint, game, shipCount);
         player = false;
 
-        while (game) {                                                           //taking turns
+        while (game) {
             if (player) {
                 gameBoardAway.playGame(boardSizeint, game, shipCount);
                 player = false;
-            } else if (!player) {
+            } else {
                 gameBoardHome.playGame(boardSizeint, game, shipCount);
                 player = true;
             }
         }
 
+        // Close the database connection when the game ends
+        battleshipDatabase.closeConnection();
     }
-    }
+}
+
     
